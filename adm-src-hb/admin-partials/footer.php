edd7eb22d1854a13465652264ae376f035a9bd22
joseph <witsdeveloper@outlook.com>
2020-06-27 18:34:35 +0300

    Local Changes

--- adm-src-hb/admin-partials/footer.php
+++ adm-src-hb/admin-partials/footer.php
@@ -2,7 +2,9 @@
     <div class="row">
         <div class="col-md-12 text-center">
             <span class="copy_sign">&copy; </span>  2020 HAPPYBOX
-           <a href="#top" id="goTop"><i class="fas fa-angle-up"></i></a>
+           <a href="#top" id="goTop"><!--<i class="fas fa-angle-up"></i-->
+              <img src="img/icn-rounded-arrow-white.svg">
+           </a>
             
         </div>    
         
--- adm-src-hb/admin-partials/nav.php
+++ adm-src-hb/admin-partials/nav.php
@@ -1,11 +1,13 @@
 <?php 
   $user_info = json_decode($_SESSION['usr_info']);
   // print_r($user_info);
-  $profile_pic_  = $util->AppUploads() . 'profiles/default.jpg';
+  //$profile_pic_  = $util->AppUploads() . 'profiles/default.jpg';
   $_name = json_decode($_SESSION['usr'])->user->username;
-  if($user_info->data->picture != 'default.jpg'){
+ /* if($user_info->data->picture != 'default.jpg'){
     $profile_pic_  = $user_info->data->picture;
-  }
+  }*/
+  $profile_pic_="img/admin_user_icon.svg";
+  $dropdown_user_icon="img/drop_user.svg";
   if($user_info->data->id > 0){
     $_name = $user_info->data->fname . ' ' . $user_info->data->sname;
   }
@@ -20,33 +22,35 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="nav navbar-nav ml-auto">
            <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-       <?=$_name?> <i class="fas fa-angle-down"></i> <img src="<?=$profile_pic_?>" class="user_icon rounded-circle">
+      <a class="nav-link dropdown-toggle admin_name_txt" href="#" id="navbardrop" data-toggle="dropdown">
+     Admin User Name <i class="fas fa-angle-down"></i> <img src="<?=$profile_pic_?>" class="user_icon rounded-circle">
       </a>
       <div class="dropdown-menu">
           <div class="card">
                <div class="card-body">
                    <div class="row">
                   <div class="col-2">
-                     <span><img src="<?=$profile_pic_?>" class="dropdown_user_img rounded-circle"></span>
+                     <span><img src="<?=$dropdown_user_icon?>" class="dropdown_user_img rounded-circle"></span>
                   </div>
                    <div class="col-10">
-                   <span><?=$_name?></span>
+                   <span class="user_dropdown_txt">Admin User Name</span>
                    <span class="email_dropdown">
-                   (<?=json_decode($_SESSION['usr'])->user->email?>)
+                   (name.surname@happybox.ke)
                    </span>
                   </div>
                       </div>
+                  
                    
              
                </div>
               <div class="card-footer">
-                  <div class="row">
-                  <div class="col-6 drop_down_profile">
-                      <a href="<?=$util->AdminHome()?>/admin-profile.php">EDIT PROFILE</a>
+                                    <div class="row text-center row_no_margin">
+                  <div class="col-6 drop_down_profile drop_down_footer_col">
+                     <a href="<?=$util->AdminHome()?>/admin-profile.php">EDIT PROFILE</a>
                   </div>
-                   <div class="col-6 drop_down_logout">
-                   <a href="<?=$util->AdminHome()?>/exit.php">LOGOUT</a>
+                   <div class="col-6 drop_down_logout drop_down_footer_col">
+                                   <a href="<?=$util->AdminHome()?>/exit.php">LOGOUT</a>
+
                   </div>
                       </div>
               </div>
