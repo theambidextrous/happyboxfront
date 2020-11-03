cd7a7f693f7937e4fc6507980172067d4a112c14
joseph <witsdeveloper@outlook.com>
2020-11-03 11:31:27 +0300

    Email Updates

---
+++ emails/customer-welcome.php
@@ -0,0 +1,202 @@
+<!--replace http://localhost/happyboxfront/emails/ to the correct directory structure -->
+<html>
+    <head>
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <title>The Happy Box:: Customer Welcome</title>
+        <style>
+            /*universal */
+            img{
+                margin:0;
+                padding:0;
+                max-width:100%;
+                display:block;
+                border:none;
+                outline:none;
+            }
+            @media (max-width: 767px){
+                .mob_100{
+                width: 100% !important;
+    padding-left: 10px !important;
+    padding-right: 10px !important;
+                }
+                .partner_img,.partner_des{
+                   
+                }
+                .partner_des {
+    width: 87%;
+   
+    margin-top: 11px;
+    padding-left: 9px;
+   }
+   .mob_auto_img{
+     margin: auto !important;
+   }
+   .mob_95 {
+      width: 95% !important;
+    padding-left: 10px !important;
+    padding-right: 10px !important;
+    
+                
+            }
+            .mob_hide{
+                display: none;
+            }
+            .desk_hide{
+           display: block;
+            }
+            .our_part_img{
+                
+    margin-bottom: 8px;
+
+            }
+            }
+            @media (min-width: 768px){
+                 .mob_hide{
+                display: block;
+            }
+             .desk_hide{
+           display: none;
+            }
+                
+            }
+              /*end universal */
+              
+        </style>
+    </head>
+    <body leftmargin="0" topmargin="0" marginwidth="0" marginheight="0" style="margin:0 auto;padding:0; font-family: Calibri;font-size: 14px;">
+        <div border="0" cellpadding="0" cellspacing="0" style="max-width:800px;width:100%;margin:0 auto;padding:0; overflow-x: hidden;">
+            <div style="width:100%; text-align: center; color: #707070;">Can’t view this email? View Online</div>
+
+            <div style="width:100%;">
+                <a href="https://happybox.ke/" target="_blank">
+                    <img src="http://localhost/happyboxfront/emails/img/on_boarding_header.svg" alt="" style="padding-bottom: 15px; width: 100%; "></a>
+            </div>
+
+            <div style="width:80%; margin: 10px auto;" class="mob_100">
+                <h3 style="text-align: center;font: normal normal bold 20px/24px Calibri;letter-spacing: 0px;color: #00ACB3">Dear [Customer Name]</h3>
+           
+            </div>
+            <div style="width:80%; margin: 24px auto;" >
+
+                <img src="http://localhost/happyboxfront/emails/img/welcome_cong.svg" alt="" style="width: 100%;">
+            </div>
+               <div style="width:80%; margin: 10px auto; text-align: center;" >
+                   <p>
+                       You now have access to an easy going gifting experience.  </p>
+                   <p>Through your account you can:
+                       
+                   </p>
+                     
+            </div>
+                     
+         <div style="width:80%; margin: 10px auto;" >
+
+             <table  style="margin-top:20px; margin-bottom: 40px;" >
+                    
+                    <tr>
+                         <td style="width:12%;vertical-align: middle;">                          
+                           <img src="http://localhost/happyboxfront/emails/img/icn-partner-portal-function-03.svg" alt="" style="">                    
+                        </td>
+                        <td style="vertical-align: middle;">
+                        <p style="color:#0185B6;padding-left: 7px;">
+                     Monitor the boxes you have been gifted and check your order history.
+                           </p>
+                        </td> </tr>
+                    <tr>
+                        <td colspan="2" style="height:20px">                          
+                                            
+                        </td>
+                         </tr>
+                      <tr>
+                         <td style="width:12%;vertical-align: middle;">                          
+                           <img src="http://localhost/happyboxfront/emails/img/icn-partner-portal-function-01.svg" alt="" style="">                    
+                        </td>
+                        <td style="vertical-align: middle;">
+                        <p style="color:#00ACB3;padding-left: 7px;">
+                Look up all the partners that correspond to your boxes.
+                           </p>
+                        </td> </tr>
+                        <tr>
+                        <td colspan="2" style="height:20px">                          
+                                            
+                        </td>
+                         </tr>
+                         <tr>
+                         <td style="width:12%;vertical-align: middle;">                          
+                           <img src="http://localhost/happyboxfront/emails/img/welcom_3.png" alt="" style="">                    
+                        </td>
+                        <td style="vertical-align: middle;">
+                        <p style="color:#FA683D;padding-left: 7px;">
+            Give us your feedback on the experiences you have enjoyed, because <br>your feedback is paramount to us.
+                           </p>
+                        </td> </tr>
+             
+                </table>
+             <br>
+            </div>
+           
+             <div style="width:80%; margin: 10px auto;">
+                 <p style="text-align:center;">
+                     Click on the link below to activate your account and start enjoying your<br> HappyBox experience!  
+                 </p>
+
+            </div>
+             <div style="width:100%">
+                <div style="">
+                    <img src="http://localhost/happyboxfront/emails/img/btn-user-activate.svg" style=" margin: 11px auto; height: 56px;" alt="">
+                </div> 
+
+            </div>
+            <div style="width:80%; margin: 10px auto;">
+                <div style="">
+                    <img src="http://localhost/happyboxfront/emails/img/welcome_happy.svg" style="margin: 17px auto;" alt="">
+                </div> 
+
+            </div>
+            
+            
+            
+            <div style="width:100%">
+                <div style="">
+                    <img src="http://localhost/happyboxfront/emails/img/Group5286@2x.png" style=" margin: 0px auto;" alt="">
+                </div> 
+
+            </div>
+            <div style="width: 70%; background: #C20A2B 0% 0% no-repeat padding-box;border-bottom-right-radius: 13px;
+                 border-bottom-left-radius: 13px;        
+                 padding: 9px 6px;
+                 height: 39px;
+                 margin: auto;" class="mob_95">
+                <div style="width:50%;float: left">
+                    <img src="http://localhost/happyboxfront/emails/img/hb-alt-logo-white.svg" style=" display: inline; " alt="">
+                    <img src="http://localhost/happyboxfront/emails/img/Chooseyourgift.svg" style=" display: inline; " alt="">
+
+                </div><div style="width: 50%; float: left;text-align: right;">
+                    <a href="https://www.facebook.com/HappyBoxke-104873668046223" target="_blank"><img src="http://localhost/happyboxfront/emails/img/icn-fb-white.svg" style=" display: inline; height: 26px;margin-left: 19px; " alt=""></a>
+                    <a href="https://www.instagram.com/happybox.ke/" target="_blank"><img src="http://localhost/happyboxfront/emails/img/icn-ig-white.svg" style="display: inline; height: 26px;margin-left: 19px; " alt=""></a>
+                    <a href="https://www.linkedin.com/company/happybox-ke/" target="_blank"><img src="http://localhost/happyboxfront/emails/img/icn-li-white.svg" style="display: inline; height: 26px;margin-left: 19px;" alt=""></a>
+
+
+                </div> 
+
+            </div>
+            <div style="width:100%;float: left;">
+                <div style="    text-align: center; margin-bottom: 10px; margin-top: 10px;">
+                    <a href="https://happybox.ke/staging/clt-src-hb/user-login.php" style="text-align: center; text-decoration: underline;font: normal normal normal 14px/20px Calibri;letter-spacing: 0px;color: #999999;" target="_blank">Login to your Account |</a> <a href="https://happybox.ke/staging/clt-src-hb/terms.php" style="text-align: center; text-decoration: underline;font: normal normal normal 14px/20px Calibri;letter-spacing: 0px;color: #999999;" target="_blank">View our Terms & Conditions |</a>
+                    <a href="" style="text-align: center; text-decoration: underline;font: normal normal normal 14px/20px Calibri;letter-spacing: 0px;color: #999999;" target="_blank">Unsubscribe</a> 
+                </div> 
+
+            </div>
+            <div style="    width: 100%;    text-align: center; margin-bottom: 30px;  letter-spacing: 0px;    color: #999999;    float: left;    line-height: 20px;">
+                You are receiving this email because you are a valued customer of HappyBox & opted in to receive email communications when you created your account. To unsubscribe from HappyBox.ke email communications, click here. HappyBox | P.O Box 30275| 00100 GPO | Nairobi | Kenya Need some help? Have a question? Please send us an email at customercare@happybox.ke
+
+            </div>
+
+
+
+
+        </div>
+
+    </body>
+</html>
