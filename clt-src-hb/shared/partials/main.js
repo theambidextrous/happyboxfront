4a78e5b6bf232a4cb6d200ad4726ec4f26eba75a
Sammy M. Waweru <sammy@witstechnologies.co.ke>
2020-11-05 13:42:41 +0300

    Modified main.js file with currencyNumberFormat()

--- clt-src-hb/shared/partials/main.js
+++ clt-src-hb/shared/partials/main.js
@@ -163,26 +163,22 @@ $('.menu-close').click(function() {
 }
 );
 
-function currencyNumberFormat(amount) {
-	var delimiter = ","; // replace comma if desired
-	var a = amount.split('.',2)
-	var d = a[1];
-	var i = parseInt(a[0]);
-	if(isNaN(i)) { return ''; }
+//Format number to 2 decimal points with comma on thousand
+function CurrencyFormatted(amount) {
+	var i = parseFloat(amount);
+	if(isNaN(i)) { i = 0.00; }
 	var minus = '';
 	if(i < 0) { minus = '-'; }
 	i = Math.abs(i);
-	var n = new String(i);
-	var a = [];
-	while(n.length > 3) {
-		var nn = n.substr(n.length-3);
-		a.unshift(nn);
-		n = n.substr(0,n.length-3);
-	}
-	if(n.length > 0) { a.unshift(n); }
-	n = a.join(delimiter);
-	if(d.length < 1) { amount = n; }
-	else { amount = n + '.' + d; }
-	amount = minus + amount;
-	return amount;
+	i = parseInt((i + .005) * 100);
+	i = i / 100;
+	s = new String(i);
+	if(s.indexOf('.') < 0) { s += '.00'; }
+	if(s.indexOf('.') == (s.length - 2)) { s += '0'; }
+	s = minus + s;
+	return s;
+}
+
+function currencyNumberFormat(amount = CurrencyFormatted(amount)) {
+	return parseFloat(amount).toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
 }
