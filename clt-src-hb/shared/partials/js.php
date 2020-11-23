6b18ce4cecd7d89e7fc6c6187327ab2f1775de45
Sammy M. Waweru <sammy@witstechnologies.co.ke>
2020-11-23 21:14:01 +0300

    Deactivated popper.js

--- clt-src-hb/shared/partials/js.php
+++ clt-src-hb/shared/partials/js.php
@@ -1,5 +1,5 @@
 <script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/popper.min.js"></script>
+<!--<script src="vendor/bootstrap/js/popper.min.js"></script>-->
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="vendor/bootstrap/js/wt.js"></script>
 <script src="vendor/owl/owl.carousel.js"></script>
