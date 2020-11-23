71d47bad65da923ead3999b5b3a460b802cc9573
Sammy M. Waweru <sammy@witstechnologies.co.ke>
2020-11-23 19:42:38 +0300

    Updated homepage slider active icon on load

--- clt-src-hb/shared/partials/css.php
+++ clt-src-hb/shared/partials/css.php
@@ -1,9 +1,7 @@
- <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
- <link href="vendor/fa5/css/all.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="vendor/owl/owl.carousel.min.css">
- <link rel="stylesheet" href="vendor/owl/owl.theme.default.css">
-  <link  rel="stylesheet" href="vendor/custom/custom-css.css" >
-  <style>
-  
-  
-  </style>
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="vendor/fa5/css/all.min.css">
+<!--/*
+<link rel="stylesheet" href="vendor/owl/owl.carousel.min.css">
+<link rel="stylesheet" href="vendor/owl/owl.theme.default.css">
+*/-->
+<link rel="stylesheet" href="vendor/custom/custom-css.css" >
--- clt-src-hb/shared/partials/js.php
+++ clt-src-hb/shared/partials/js.php
@@ -1,16 +1,15 @@
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="vendor/bootstrap/js/wt.js"></script>
-    <script src="vendor/owl/owl.carousel.js"></script>
-      <!-- jQuery Tinymce -->
-  <script src="<?=$util->AppHome()?>/adm-src-hb/vendor/tinymce/jquery.tinymce.min.js"></script>
-  <script src="<?=$util->AppHome()?>/adm-src-hb/vendor/tinymce/tinymce.min.js"></script>
-        <script src="<?=$util->ClientHome()?>/shared/partials/main.js"></script>
- 
-    
-    <script>
-
-
-</script>
-      
-     
+<!--/*
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="vendor/bootstrap/js/wt.js"></script>
+<script src="vendor/owl/owl.carousel.js"></script>
+*/-->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/popper.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="vendor/bootstrap/js/wt.js"></script>
+<!-- jQuery Tinymce -->
+<script src="<?=$util->AppHome()?>/adm-src-hb/vendor/tinymce/jquery.tinymce.min.js"></script>
+<script src="<?=$util->AppHome()?>/adm-src-hb/vendor/tinymce/tinymce.min.js"></script>
+<script src="<?=$util->ClientHome()?>/shared/partials/main.js"></script>
