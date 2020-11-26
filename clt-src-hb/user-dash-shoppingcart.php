0012988debbe4982ca2db565888b8505691e3885
joseph <witsdeveloper@outlook.com>
2020-11-26 13:28:58 +0300

    nov 26 update

--- clt-src-hb/user-dash-shoppingcart.php
+++ clt-src-hb/user-dash-shoppingcart.php
@@ -130,7 +130,8 @@ $util->ShowErrors(1);
 						<td colspan="2">KES <?=number_format((array_sum($_total_cart)+$_total_shipping), 2)?></td>
 					</tr>
 					<tr align="right" class="cart_totals tr_border_top cart_totals_actions">
-						<td colspan="6"><a href="<?=$util->ClientHome()?>"><img src="shared/img/btn-continue-shopping.svg"></a> <a href="<?=$util->ClientHome()?>/user-dash-shipping.php"><img src="shared/img/btn-shipping-method-blue.svg"></a></td>
+						<td colspan="6"><a href="<?=$util->ClientHome()?>"><img src="shared/img/btn-continue-shopping.png"></a> 
+                                                    <a href="<?=$util->ClientHome()?>/user-dash-shipping.php"><img src="shared/img/btn-shipping-method-blue.png"></a></td>
 					</tr>
 				</table>
 			</div>
@@ -227,7 +228,9 @@ $util->ShowErrors(1);
 					</tr>
 					<tr align="right" class="cart_totals tr_border_top cart_totals_actions">
 						<td colspan="2 ">
-                                                    <a href="<?=$util->ClientHome()?>"><img src="shared/img/btn-continue-shopping.svg"></a> <a href="<?=$util->ClientHome()?>/user-dash-shipping.php"><img src="shared/img/btn-shipping-method-blue.svg"></a></td>
+                                                    <a href="<?=$util->ClientHome()?>"><img src="shared/img/btn-continue-shopping.png"></a> 
+                                                    <a href="<?=$util->ClientHome()?>/user-dash-shipping.php"><img src="shared/img/btn-shipping-method-blue.png"></a>
+                                                </td>
 					</tr>
 				</table>
 			</div>
