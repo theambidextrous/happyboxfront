9dbfbf00d09bcadd6517f8b41ee5501f38635d73
Soft Bucket IO <softbucket.io@gmail.com>
2021-07-11 18:19:12 +0300

    rating changes

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $picture = new Picture();
 $inventory = new Inventory();
 $util->ShowErrors(1);
 $_all_boxes = json_decode($box->get_all_active('0'), true)['data'];
+$util->Show($box->get_all_active('0'));
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -54,6 +55,7 @@ $_all_boxes = json_decode($box->get_all_active('0'), true)['data'];
 					<li data-target="#desktop_carousel" data-slide-to="0" class="active"></li>
 					<li data-target="#desktop_carousel" data-slide-to="1"></li>
 					<li data-target="#desktop_carousel" data-slide-to="2"></li>
+					<li data-target="#desktop_carousel" data-slide-to="3"></li>
 				</ul>
 				<div class="carousel-inner">
 					<div class="carousel-item slider_1 active">
